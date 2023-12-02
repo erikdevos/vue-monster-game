d8d7e4994e2b4619568a41261c684df2fb1faa18
erikdevos <erikdevos87@gmail.com>
2023-12-02 23:42:53 +0100

    Add additional game logic for the various screens.

--- scripts.js
+++ scripts.js
@@ -3,6 +3,39 @@ function getrandomvalue(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function calculateDamage(attackerType, moveType, opponentType) {
+    const typeChart = {
+        Normal: { Normal: 1, Fighting: 2, Flying: 1, Poison: 1, Ground: 1, Rock: 0.5, Bug: 1, Ghost: 0, Steel: 0.5, Fire: 1, Water: 1, Grass: 1, Electric: 1, Psychic: 1, Ice: 1, Dragon: 1, Dark: 1, Fairy: 1 },
+        Fighting: { Normal: 0.5, Fighting: 1, Flying: 0.5, Poison: 0.5, Ground: 1, Rock: 2, Bug: 0.5, Ghost: 0, Steel: 2, Fire: 1, Water: 1, Grass: 1, Electric: 1, Psychic: 0.5, Ice: 2, Dragon: 1, Dark: 2, Fairy: 0.5 },
+        Flying: { Normal: 1, Fighting: 2, Flying: 1, Poison: 1, Ground: 1, Rock: 0.5, Bug: 2, Ghost: 1, Steel: 0.5, Fire: 1, Water: 1, Grass: 0.5, Electric: 2, Psychic: 1, Ice: 2, Dragon: 1, Dark: 1, Fairy: 1 },
+        Poison: { Normal: 1, Fighting: 1, Flying: 1, Poison: 0.5, Ground: 2, Rock: 1, Bug: 1, Ghost: 1, Steel: 0, Fire: 1, Water: 1, Grass: 0.5, Electric: 1, Psychic: 1, Ice: 1, Dragon: 1, Dark: 1, Fairy: 2 },
+        Ground: { Normal: 1, Fighting: 1, Flying: 0, Poison: 2, Ground: 1, Rock: 2, Bug: 0.5, Ghost: 1, Steel: 2, Fire: 2, Water: 1, Grass: 0.5, Electric: 2, Psychic: 1, Ice: 1, Dragon: 1, Dark: 1, Fairy: 1 },
+        Rock: { Normal: 1, Fighting: 0.5, Flying: 2, Poison: 1, Ground: 0.5, Rock: 1, Bug: 2, Ghost: 1, Steel: 0.5, Fire: 2, Water: 1, Grass: 1, Electric: 1, Psychic: 1, Ice: 2, Dragon: 1, Dark: 1, Fairy: 1 },
+        Bug: { Normal: 1, Fighting: 0.5, Flying: 0.5, Poison: 1, Ground: 1, Rock: 1, Bug: 1, Ghost: 0.5, Steel: 0.5, Fire: 0.5, Water: 1, Grass: 2, Electric: 1, Psychic: 2, Ice: 1, Dragon: 1, Dark: 2, Fairy: 0.5 },
+        Ghost: { Normal: 0, Fighting: 0, Flying: 1, Poison: 1, Ground: 1, Rock: 1, Bug: 1, Ghost: 2, Steel: 1, Fire: 1, Water: 1, Grass: 1, Electric: 1, Psychic: 1, Ice: 1, Dragon: 1, Dark: 0.5, Fairy: 1 },
+        Steel: { Normal: 0.5, Fighting: 2, Flying: 0.5, Poison: 0, Ground: 2, Rock: 0.5, Bug: 0.5, Ghost: 1, Steel: 0.5, Fire: 2, Water: 1, Grass: 0.5, Electric: 1, Psychic: 0.5, Ice: 0.5, Dragon: 0.5, Dark: 0.5, Fairy: 0.5 },
+        Fire: { Normal: 1, Fighting: 1, Flying: 1, Poison: 1, Ground: 1, Rock: 0.5, Bug: 2, Ghost: 1, Steel: 0.5, Fire: 0.5, Water: 2, Grass: 0.5, Electric: 1, Psychic: 1, Ice: 2, Dragon: 1, Dark: 1, Fairy: 0.5 },
+        Water: { Normal: 1, Fighting: 1, Flying: 1, Poison: 1, Ground: 1, Rock: 2, Bug: 1, Ghost: 1, Steel: 0.5, Fire: 0.5, Water: 0.5, Grass: 2, Electric: 1, Psychic: 1, Ice: 1, Dragon: 1, Dark: 1, Fairy: 1 },
+        Grass: { Normal: 1, Fighting: 1, Flying: 2, Poison: 2, Ground: 0.5, Rock: 0.5, Bug: 0.5, Ghost: 1, Steel: 2, Fire: 2, Water: 0.5, Grass: 0.5, Electric: 1, Psychic: 1, Ice: 2, Dragon: 1, Dark: 1, Fairy: 1 },
+        Electric: { Normal: 1, Fighting: 1, Flying: 0.5, Poison: 1, Ground: 2, Rock: 1, Bug: 1, Ghost: 1, Steel: 0.5, Fire: 1, Water: 1, Grass: 1, Electric: 0.5, Psychic: 1, Ice: 1, Dragon: 1, Dark: 1, Fairy: 1 },
+        Psychic: { Normal: 1, Fighting: 2, Flying: 1, Poison: 2, Ground: 1, Rock: 1, Bug: 1, Ghost: 1, Steel: 0.5, Fire: 1, Water: 1, Grass: 1, Electric: 1, Psychic: 0.5, Ice: 1, Dragon: 1, Dark: 0, Fairy: 1 },
+        Ice: { Normal: 1, Fighting: 1, Flying: 2, Poison: 1, Ground: 2, Rock: 1, Bug: 1, Ghost: 1, Steel: 0.5, Fire: 0.5, Water: 0.5, Grass: 2, Electric: 1, Psychic: 1, Ice: 0.5, Dragon: 2, Dark: 1, Fairy: 1 },
+        Dragon: { Normal: 1, Fighting: 1, Flying: 1, Poison: 1, Ground: 1, Rock: 1, Bug: 1, Ghost: 1, Steel: 0.5, Fire: 1, Water: 1, Grass: 1, Electric: 1, Psychic: 1, Ice: 2, Dragon: 2, Dark: 1, Fairy: 0 },
+        Dark: { Normal: 1, Fighting: 0.5, Flying: 1, Poison: 1, Ground: 1, Rock: 1, Bug: 1, Ghost: 2, Steel: 1, Fire: 1, Water: 1, Grass: 1, Electric: 1, Psychic: 2, Ice: 1, Dragon: 1, Dark: 0.5, Fairy: 2 },
+        Fairy: { Normal: 1, Fighting: 2, Flying: 1, Poison: 0.5, Ground: 1, Rock: 1, Bug: 1, Ghost: 1, Steel: 2, Fire: 1, Water: 1, Grass: 1, Electric: 1, Psychic: 1, Ice: 1, Dragon: 0, Dark: 0.5, Fairy: 1 },
+    };
+
+    const effectiveness = typeChart[moveType][opponentType];
+    const damageMultiplier = effectiveness === undefined ? 1 : effectiveness;
+
+    // Assuming base damage and other factors, you can adjust this based on your game's mechanics.
+    const baseDamage = 50;
+    const totalDamage = baseDamage * damageMultiplier;
+
+    return totalDamage;
+}
+
+
 const app = Vue.createApp({
 	data() {
 		const enemyMonsters = [
@@ -125,12 +158,14 @@ const app = Vue.createApp({
 			winner: null,
 			logMessages: [],
 			playerMonsterName: "Pikachu",
-			playerName: '',
+			playerName: 'Ash',
 			playerAttacked: false,
 			monsterAttacked: false,
 			specialAttackAttacked: false,
 			healAnimation: false,
-			surrenderAnimation: false
+			surrenderAnimation: false,
+			playerSelect: true,
+			monsterSelect: null
 		};
 	},
 
@@ -156,14 +191,14 @@ const app = Vue.createApp({
 	methods: {
 		startGame() {
 			this.gameStarted = true;
+			this.clearLog();
 			this.playerHealth = 100;
 			this.monsterHealth = 100;
 			this.enemyMonsterHealth = 0;
 			this.winner = null;
 			this.currentRound = 0;
 			this.surrenderAnimation = false;
-			this.clearLog();
-			console.log("StartGame")
+			this.monsterSelect = false;	
 
         if (this.selectedMonster) {
         this.currentEnemyMonster = this.selectedMonster;
@@ -178,6 +213,7 @@ const app = Vue.createApp({
 		
         selectMonster(monster) {
             this.selectedMonster = monster;
+			this.monsterSelect = false;
             this.startGame();
         },
 
@@ -196,6 +232,11 @@ const app = Vue.createApp({
 				"enemyMonsterHealth",
 				this.enemyMonsterHealth
 			);
+
+			if (this.playerHealth <= 0) {
+				// Player's health is already 0 or below, no further attacks allowed
+				return;
+			}
 
 			this.currentRound++;
 			const playerAttackValue = getrandomvalue(5, 12);
@@ -269,6 +310,9 @@ const app = Vue.createApp({
 
 		resetGame() {
 			this.gameStarted = false;
+			this.playerSelect = true;
+			this.isLogEmpty = true;
+			this.clearLog();
 		},
 
 		addLogMessage(who, what, value) {
@@ -303,6 +347,12 @@ const app = Vue.createApp({
 		clearLog() {
 			this.logMessages = [];
 		},
+
+		confirmPlayer() {
+			this.playerSelect = false;
+			this.monsterSelect = true;
+			console.log("Player selected true");
+		}
 	},
 
 	computed: {
