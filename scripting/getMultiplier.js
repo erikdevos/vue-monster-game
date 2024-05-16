5b8efa421cb2b6cb7955eb5b0a82c84a84eeecfd
Erik de Vos <e.vos@nexwork.nl>
2024-05-16 15:25:51 +0200

    Fix water vs fire type multiplier.

--- scripting/getMultiplier.js
+++ scripting/getMultiplier.js
@@ -10,7 +10,7 @@ function getMultiplier(attackerType, opponentType) {
       ghost: { normal: 0, fighting: 0, flying: 1, poison: 1, ground: 1, rock: 1, bug: 1, ghost: 2, steel: 1, fire: 1, water: 1, grass: 1, electric: 1, psychic: 1, ice: 1, dragon: 1, dark: 0.5, fairy: 1 },
       steel: { normal: 0.5, fighting: 2, flying: 0.5, poison: 0, ground: 2, rock: 0.5, bug: 0.5, ghost: 1, steel: 0.5, fire: 2, water: 1, grass: 0.5, electric: 1, psychic: 0.5, ice: 0.5, dragon: 0.5, dark: 0.5, fairy: 0.5 },
       fire: { normal: 1, fighting: 1, flying: 1, poison: 1, ground: 1, rock: 0.5, bug: 2, ghost: 1, steel: 0.5, fire: 2, water: 0.5, grass: 2, electric: 1, psychic: 1, ice: 2, dragon: 1, dark: 1, fairy: 0.5 },
-      water: { normal: 1, fighting: 1, flying: 1, poison: 1, ground: 1, rock: 2, bug: 1, ghost: 1, steel: 0.5, fire: 0.5, water: 0.5, grass: 2, electric: 1, psychic: 1, ice: 1, dragon: 1, dark: 1, fairy: 1 },
+      water: { normal: 1, fighting: 1, flying: 1, poison: 1, ground: 1, rock: 2, bug: 1, ghost: 1, steel: 0.5, fire: 2, water: 0.5, grass: 2, electric: 1, psychic: 1, ice: 1, dragon: 1, dark: 1, fairy: 1 },
       grass: { normal: 1, fighting: 1, flying: 2, poison: 2, ground: 0.5, rock: 0.5, bug: 0.5, ghost: 1, steel: 2, fire: 0.5, water: 0.5, grass: 0.5, electric: 1, psychic: 1, ice: 2, dragon: 1, dark: 1, fairy: 1 },
       electric: { normal: 1, fighting: 1, flying: 0.5, poison: 1, ground: 2, rock: 1, bug: 1, ghost: 1, steel: 0.5, fire: 1, water: 1, grass: 1, electric: 0.5, psychic: 1, ice: 1, dragon: 1, dark: 1, fairy: 1 },
       psychic: { normal: 1, fighting: 2, flying: 1, poison: 2, ground: 1, rock: 1, bug: 1, ghost: 1, steel: 0.5, fire: 1, water: 1, grass: 1, electric: 1, psychic: 0.5, ice: 1, dragon: 1, dark: 0, fairy: 1 },
